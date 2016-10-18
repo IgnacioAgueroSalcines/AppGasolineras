f2b0abb64351e40f27a4c126ac0c1e5863617624
David <davidberbil@hotmail.com>
2016-10-18 08:54:54 +0200

    Commit de prueba

--- app/src/main/java/com/example/nachoaguero/appgasolineras/Negocio/DatosGasolineras.java
+++ app/src/main/java/com/example/nachoaguero/appgasolineras/Negocio/DatosGasolineras.java
@@ -3,6 +3,13 @@ package com.example.nachoaguero.appgasolineras.Negocio;
 /**
  * Created by deivid on 18/10/16.
  */
+import android.util.Log;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.List;
+
 
 public class DatosGasolineras {
 }
