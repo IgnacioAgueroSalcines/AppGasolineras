8f580141597006bfebca3f38f4d57eb85d257d45
David <davidberbil@hotmail.com>
2016-10-25 16:03:44 +0200

    Generados Test de Integración para el caso de mostrar la lista, y el caso de no tener acceso a una conexión (sin tener datos previos almacenados a los que recurrir)

---
+++ app/src/androidTest/java/com/example/nachoaguero/appgasolineras/Presentacion/ListaGasolinerasActivityTest.java
@@ -0,0 +1,67 @@
+package com.example.nachoaguero.appgasolineras.Presentacion;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.nachoaguero.appgasolineras.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ListaGasolinerasActivityTest {
+
+    @Rule
+    public ActivityTestRule<ListaGasolinerasActivity> mActivityTestRule = new ActivityTestRule<>(ListaGasolinerasActivity.class);
+
+    @Test
+    public void listaGasolinerasActivityTest() {
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.image), withContentDescription("Logo"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.customListView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
