01c87220d5c567a234b8972fc02b284b7199d760
Ignacio Agüero Salcines <ias61@alumnos.unican.es>
2016-10-26 14:34:22 +0200

    Merge commit '681bc4a391598cd0c4dd80f6e33f45b6f13dadda' into HEAD
    # Conflicts:
    #	app/src/main/java/com/example/nachoaguero/appgasolineras/Presentacion/ListaGasolinerasActivity.java

--- app/src/main/java/com/example/nachoaguero/appgasolineras/Datos/GasolineraDAO.java
+++ app/src/main/java/com/example/nachoaguero/appgasolineras/Datos/GasolineraDAO.java
@@ -1,10 +1,13 @@
 package com.example.nachoaguero.appgasolineras.Datos;
 
+import android.content.Context;
 import android.util.Log;
 
+import com.example.nachoaguero.appgasolineras.Utilities.FilesOperations;
 import com.example.nachoaguero.appgasolineras.Utilities.ParserJSON;
 import com.example.nachoaguero.appgasolineras.Utilities.RemoteFetch;
 
+import java.io.BufferedInputStream;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,6 +39,22 @@ public class GasolineraDAO implements IGasolineraDAO{
             throw new RuntimeException(e);
         }
     }//obtenGasolineras
+
+    public boolean obtenGasolinerasSinconexion(){
+        try {
+
+            remoteFetch.readBuffer(context);
+            BufferedInputStream buffer=remoteFetch.getBufferedDataGasolineras();
+            listaGasolineras = ParserJSON.readJsonStream(buffer);
+
+            Log.d("ENTRA", "Obten gasolineras:"+listaGasolineras.size());
+            return true;
+        }catch(Exception e){
+            Log.e("ERROR","Error en la obtención de gasolineras: "+e.getMessage());
+            e.printStackTrace();
+            return false;
+        }//try
+    }//obtenGasolinerassinconexion
 
 
     public List<Gasolinera> getListaGasolineras(){
